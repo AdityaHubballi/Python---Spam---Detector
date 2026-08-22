0aadfba3fdb7ee477d7e7079fd924079db09c267
Aditya.Hubballi <adityahubballi511@gmail.com>
2026-08-22 17:11:01 +0530

    Update python spam_detector.py

--- python spam_detector.py
+++ python spam_detector.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
